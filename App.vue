bb26719245dc3308d9c23dc4217d5cc8d0c451b0
九月三秋 <993623938@qq.com>
2019-06-12 23:52:33 +0800

    增加通用样式

--- App.vue
+++ App.vue
@@ -14,4 +14,33 @@
 
 <style>
 	/*每个页面公共css */
+	@import "common/main.css";
+	@import "common/icon.css";
+	@import "common/bootstrap.wxss";
+	page {
+		background-color: #f7f7f7;
+	}
+	
+	.text-primary {
+		color: #09bb07 !important;
+	}
+	
+	.fixed-bottom {
+		position: fixed;
+		right: 0;
+		bottom: 0;
+		left: 0;
+	}
+	
+	.text-link {
+		color: #576b95;
+	}
+	
+	.text-success {
+		color: #09BB07;
+	}
+	
+	.text-error{
+		color: #e64340;
+	}
 </style>
