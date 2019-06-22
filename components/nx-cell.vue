691ca53947fd86284f046712934ea3c15931e812
九月三秋 <993623938@qq.com>
2019-06-23 02:38:39 +0800

    我的增加list

---
+++ components/nx-cell.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="d-flex nx-cell" hover-class="nx-cell-active">
+		<view class="nx-cell-left d-flex align-items-center">
+			<image class="nx-cell-icon" :src="icon"></image>
+		</view>
+		<view class="nx-cell-right flex-grow-1 d-flex" :class="border?'border-b':''">
+			<view class="flex-grow-1">
+				<text>{{title}}</text>
+			</view>
+			<text class="cuIcon-right text-gray"></text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		props: {
+			icon: String,
+			title: String,
+			border: {
+				type: Boolean,
+				value: true
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.nx-cell {
+		background: white;
+		color: #353535;
+	}
+
+	.nx-cell-left {
+		padding: 31upx 36upx;
+	}
+
+	.nx-cell-right {
+		font-size: 36upx;
+		padding: 31upx 26upx 31upx 0;
+	}
+
+	.nx-cell-icon {
+		width: 40upx;
+		height: 40upx;
+	}
+
+	.nx-cell-active {
+		background-color: #e5e5e5;
+	}
+
+	.border-b {
+		border-bottom: 1upx solid #eee;
+	}
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,10 +1,14 @@
 <template>
 	<view>
-		
+		<nx-cell icon="/static/disk.png" border title="我的作品"></nx-cell>
+		<button open-type="contact">
+			<nx-cell icon="/static/service.png" title="我要吐槽"></nx-cell>
+		</button>
 	</view>
 </template>
 
 <script>
+	import nxCell from '../../components/nx-cell.vue';
 	export default {
 		data() {
 			return {
@@ -13,10 +17,19 @@
 		},
 		methods: {
 			
+		},
+		components: {
+			nxCell
 		}
 	}
 </script>
 
 <style>
-
+	/* 恢复button默认值 */
+	button{
+		all:initial;
+	}
+	button:after{
+		all:inherit
+	}
 </style>
