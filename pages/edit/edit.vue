f6d31c051e5eccb3c97bf77ebf8f653929adea03
九月三秋 <993623938@qq.com>
2019-07-15 21:36:03 +0800

    编辑

--- pages/edit/edit.vue
+++ pages/edit/edit.vue
@@ -1,10 +1,30 @@
 <template>
-	<view>
-		
+	<view class="cu-bar search bg-white fixed">
+		<view class="search-form">
+			<input v-model="nikeName" class="px-2" auto-focus type="text" :placeholder="placeholder"></input>
+		</view>
+		<view class="action">
+			<button class="cu-btn bg-green shadow-blur" @tap="back">取消</button>
+		</view>
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				nikeName:'',
+				placeholder:'请输入昵称'
+			}
+		},
+		methods:{
+			back(){
+				uni.navigateBack({
+					delta:1
+				})
+			}
+		}
+	}
 </script>
 
 <style>
