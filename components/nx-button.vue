9b8b4f733ec9205b60b553251b10468f8d9dcb6a
九月三秋 <993623938@qq.com>
2019-07-28 23:03:44 +0800

    增加nx-button组件和成功页面

---
+++ components/nx-button.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="container">
+		<view class="d-flex justify-center">
+			<button :open-type="openType" :class="['btn','primary',plain?'plain':'']">
+				<slot></slot>
+			</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			};
+		}, 
+		methods:{
+			
+		},
+		props:{
+			plain:{
+				type:Boolean,
+				default:false
+			},
+			openType:{
+				type:String,
+				default:''
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	button {
+		all: initial;
+	}
+
+	button::after {
+		all: initial;
+	}
+
+	button:hover {
+		all: initial;
+	}
+
+	.container {
+		.btn {
+			width: 285px;
+			height: 44px;
+			line-height: 44px;
+			border-radius: 22px;
+			text-align: center;
+		}
+
+		.primary {
+			background-color: #09bb07;
+			color: white;
+		}
+
+		.plain {
+			background-color: white;
+			border: 1px solid #09bb07;
+			color: #09bb07;
+			box-sizing: border-box;
+		}
+	}
+</style>
--- pages/success/success.vue
+++ pages/success/success.vue
@@ -1,36 +1,50 @@
 <template>
 	<view>
-		<view class="d-flex justify-center">
-			<view>
-				<text style="font-size: 250upx;" class=" cuIcon-roundcheckfill text-green"></text>
+		<view class="text-center">
+			<image class="logo" src="../../static/star.png" mode=""></image>
+		</view>
+		<view class="success text-center mt-3">
+			<text>恭喜！节目已经发布成功</text>
+		</view>
+		<view>
+			<view class="mb-4">
+				<nxButton openType="share">分享，轻松提升播放量</nxButton>
 			</view>
+			<nxButton plain>完成</nxButton>
 		</view>
-		<view class="text-center text-xxl py-3">
-			上传成功
-		</view>
-		<view class="mx-3 mt-5">
-			<button class="weui-btn" type="primary" open-type="share">分享好友</button>
-			<button class="weui-btn text-primary" type="default" @click="goHome">返回首页</button>
+		<view class="d-flex justify-center mt-5">
+			<view class="content d-flex">
+				<view>
+					<image class="cover" src="http://iph.href.lu/60x60"></image>
+				</view>
+				<view class="right">
+					<view class="title">
+						<text>规则都是用来打破的</text>
+					</view>
+					<view class="time">
+						<text>时长：04:24</text>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import nxButton from '../../components/nx-button.vue';
 	export default {
 		data() {
-			return {
-
-			}
+			return {}
 		},
 		onShareAppMessage() {
 			let item = uni.getStorageSync('parameter');
 			return {
 				title: item.title,
 				path: '/pages/play/play?author=' + item.author +
-						'&duration=' + item.duration +
-						'&title=' + item.title +
-						'&audioUrl=' + item.audioUrl+'&isShare=true',
-				imageUrl:'../../static/share.png'
+					'&duration=' + item.duration +
+					'&title=' + item.title +
+					'&audioUrl=' + item.audioUrl + '&isShare=true',
+				imageUrl: '../../static/share.png'
 			}
 		},
 		methods: {
@@ -39,10 +53,58 @@
 					url: '../index/index'
 				})
 			}
+		},
+		components: {
+			nxButton
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	page {
+		background-color: #FFFFFF;
+		height: 100Vh;
+		width: 100vw;
+	}
 
+	.logo {
+		width: 300upx;
+		height: 300upx;
+	}
+
+	.success {
+		color: rgba(51, 51, 51, 1);
+		font-size: 17px;
+		margin-bottom: 120upx;
+	}
+
+	.content {
+		padding: 30rpx;
+		width: 325px;
+		height: 90px;
+		border-radius: 6px;
+		background-color: rgba(248, 248, 248, 1);
+
+		.cover {
+			width: 120rpx;
+			height: 120rpx;
+			border-radius: 6rpx;
+		}
+
+		.right {
+			padding: 15rpx;
+			padding-left: 30rpx;
+
+			.title {
+				color: rgba(16, 16, 16, 1);
+				font-size: 13px;
+				margin-bottom: 24rpx;
+			}
+
+			.time {
+				color: rgba(152, 152, 152, 1);
+				font-size: 13px;
+			}
+		}
+	}
 </style>
