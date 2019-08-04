399596a1b686913f52891ed523f002e55eb4251c
Messy <729578197@qq.com>
2019-08-04 23:38:16 +0800

    修改我的作品样式

--- pages/myVoice/myVoice.vue
+++ pages/myVoice/myVoice.vue
@@ -2,22 +2,22 @@
 	<view class="page">
 		<view class="pageBox">
 
-			<view class="d-flex voiceItem" v-for="(item,key,index) in voiceList" :key="item.voiceId" @tap="toPlay(item.voiceId)">
+			<view class="d-flex voiceItem" v-for="(item,key) in voiceList" :key="item._Id" @tap="toPlay(item._Id)">
 				<view class="d-flex align-center itemNumber">{{key+1}}</view>
 				<view class="d-flex flex-column justify-between itemTop">
 					<view class="d-flex justify-between align-center">
-						<view class="itemName">{{item.voiceName}}</view>
-						<view class="itemDate">{{item.voiceDate}}</view>
+						<view class="itemName">{{item.title}}</view>
+						<view class="itemDate">{{formatTime(item.createTime)}}</view>
 					</view>
 					<view class="d-flex justify-between itemBottom">
 						<view class="d-flex itemViewBox">
 							<view class="d-flex viewNumber">
 								<image src="../../static/playNumber.png" mode=""></image>
-								<view>{{item.voicePlay}}</view>
+								<view>{{item.playAmount}}</view>
 							</view>
 							<view class="d-flex itemTime">
 								<image src="../../static/time.png" mode=""></image>
-								<view>{{item.voiceTime}}</view>
+								<view>{{formatSeconds(item.duration)}}</view>
 							</view>
 						</view>
 						<view class="more">. . .</view>
@@ -35,44 +35,59 @@
 		mapGetters
 	} from 'vuex';
 	const db = wx.cloud.database();
-	const _ = db.command
+	const _ = db.command;
+	// 引入格式化时间等文件
+	let util = require('../../common/util.js');
 	export default {
 		data() {
 			return {
-				voiceList: [{
-					voiceId: 1,
-					voiceName: '你所谓的迷茫只不过是赚钱太少',
-					voiceDate: '2019-07-15',
-					voicePlay: 27,
-					voiceTime: '03:56',
-				}, {
-					voiceId: 2,
-					voiceName: '赚钱太少啦啦',
-					voiceDate: '2019-12-01',
-					voicePlay: 66,
-					voiceTime: '01:03:56',
-				}]
+				voiceList: '',
 			}
 		},
 		onLoad() {
-
+			this.getMyVioce();
 		},
 		computed: {
-			...mapState(['userInfo'])
+			...mapState(['userInfo']),
 		},
 		methods: {
+			// 根据openId获取我的作品列表
 			getMyVioce() {
 				db.collection('dynamic').where({
 					_openid: this.userInfo._openid
 				}).get().then((res) => {
 					console.log(res);
+					if (res.errMsg == 'collection.get:ok') {
+						this.voiceList = res.data;
+					} else {
+						uni.showToast({
+							title: res.errMsg,
+							icon: 'loading',
+							duration: 1500,
+						});
+					}
+
 				})
 			},
+			// 去播放页
 			toPlay(voiceId) {
 				uni.navigateTo({
-					url: `../play/play?voiceId=${voiceId}`
+					url: `../dynamicDetail/dynamicDetail?voiceId=${voiceId}`
 				})
-			}
+			},
+			// 格式化时间为yyyy-mm-dd
+			formatTime(timeStamp) {
+				let year = new Date(timeStamp).getFullYear();
+				let month = new Date(timeStamp).getMonth() + 1 < 10 ? "0" + (new Date(timeStamp).getMonth() + 1) : new Date(
+					timeStamp).getMonth() + 1;
+				let date = new Date(timeStamp).getDate() < 10 ? "0" + new Date(timeStamp).getDate() : new Date(timeStamp).getDate();
+				return `${year}-${month}-${date}`;
+			},
+			// 格式化秒数
+			formatSeconds(seconds) {
+				return util.formatDuration(seconds);
+			},
+
 		}
 	}
 </script>
