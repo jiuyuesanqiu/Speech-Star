42774c99cbea2395f121546261a09821de7f40f4
九月三秋 <993623938@qq.com>
2019-08-09 00:41:07 +0800

    增加分享海报生成

--- pages/success/success.vue
+++ pages/success/success.vue
@@ -27,52 +27,160 @@
 				</view>
 			</view>
 		</view>
+		<!-- canvas.wxml -->
+		<canvas style="width: 300px; height: 240px;position: fixed;left: 9999999upx;" canvas-id="firstCanvas"></canvas>
 	</view>
 </template>
 
 <script>
 	import util from '../../common/util.js';
 	import nxButton from '../../components/nx-button.vue';
+	import wmPoster from '../../components/wm-poster/wm-poster.vue';
+	import uploadUtil from '../../common/uploadUtil.js'
+	import {
+		mapState
+	} from 'vuex';
 	export default {
 		data() {
 			return {
-				coverSrc:'',
-				title:'',
-				duration:'',
-				id:''
+				coverSrc: '',
+				title: '',
+				duration: '',
+				id: '',
+				shareCoverUrl: ''
 			}
 		},
 		onShareAppMessage() {
 			return {
-				title: this.title,
-				path: `/pages/dynamicDetail/dynamicDetail?id=${this.id}`
+				title: this.getShareTitle(this.userInfo.nickName, this.title),
+				path: `/pages/dynamicDetail/dynamicDetail?id=${this.id}`,
+				imageUrl: this.shareCoverUrl
 			}
 		},
+
 		methods: {
+			getShareCover() {
+				return new Promise((resolve, reject) => {
+					wx.canvasToTempFilePath({
+						canvasId: 'firstCanvas',
+						success(res) {
+							resolve(res.tempFilePath)
+						}
+					})
+				})
+			},
+			/**
+			 * 获取分享标题
+			 */
+			getShareTitle(nickName, title) {
+				return `【${nickName}】${title}`;
+			},
 			goHome() {
 				uni.switchTab({
 					url: '../index/index?refresh=true'
 				})
 			},
-			toDynamicDetail(){
+			toDynamicDetail() {
 				uni.redirectTo({
-					url:`../dynamicDetail/dynamicDetail?id=${this.id}`
+					url: `../dynamicDetail/dynamicDetail?id=${this.id}`
+				})
+			},
+			async uploadFile(cloudPath, filePath, progressCallback = () => {}) {
+				return new Promise((resolve, reject) => {
+					let uploadTask = wx.cloud.uploadFile({
+						cloudPath: cloudPath,
+						filePath: filePath,
+						success(res) {
+							resolve(res.fileID)
+							console.log('上传文件成功')
+						}
+					})
+					uploadTask.onProgressUpdate(progressCallback)
+				})
+			},
+			/**
+			 * 格式化云存储路径
+			 * @param {Object} tempFilePath	临时文件路径
+			 * @param {Object} folder	上传到哪个文件夹
+			 */
+			formatCloudPath(tempFilePath, folder = '') {
+				//获取文件后缀
+				let suffix = tempFilePath.substring(tempFilePath.lastIndexOf('.'));
+				//封面生成文件名
+				let cloudPath = folder + new Date().getTime() + Math.floor(Math.random() * 100) + suffix;
+				console.log("格式化云存储路径成功")
+				return cloudPath;
+			},
+			generateShareImg() {
+				const self = this;
+				wx.canvasToTempFilePath({
+					canvasId: 'firstCanvas',
+					success(res) {
+						let cloudPath = self.formatCloudPath(res.tempFilePath, 'img/shareCover/')
+						self.uploadFile(cloudPath, res.tempFilePath).then((fileID) => {
+							wx.cloud.getTempFileURL({
+								fileList: [{
+									fileID
+								}]
+							}).then(res => {
+								self.shareCoverUrl = res.fileList[0].tempFileURL;
+								console.log("生成分享图成功", self.shareCoverUrl)
+							}).catch(error => {})
+						});
+					}
+				})
+			},
+			async getImageInfo(imgSrc) {
+				return new Promise((resolve, errs) => {
+					uni.getImageInfo({
+						src: imgSrc,
+						success: function(image) {
+							resolve(image);
+						},
+						fail(err) {
+							errs(err);
+						}
+					});
+				});
+			},
+			async dealShareImg(){
+				const self = this;
+				// 使用 wx.createContext 获取绘图上下文 context
+				var ctx = wx.createCanvasContext('firstCanvas')
+				ctx.drawImage('/static/shareCover.png', 0, 0, 300, 240)
+				//开始路径画圆,剪切处理
+				ctx.save();
+				ctx.beginPath();
+				ctx.arc(145, 131, 64, 0, Math.PI * 2);
+				ctx.clip(); //剪切路径
+				let image = await this.getImageInfo(this.userInfo.avatarUrl);
+				ctx.drawImage(image.path, 81, 67, 128, 128)
+				//恢复状态
+				ctx.restore();
+				ctx.draw(false, () => {
+					self.generateShareImg();
 				})
 			}
 		},
 		onLoad(option) {
+			const self = this;
 			this.coverSrc = option.coverSrc;
 			this.title = option.title;
 			this.duration = option.duration;
 			this.id = option.id;
 		},
-		computed:{
-			durationText(){
+		onReady() {
+			this.dealShareImg();
+		},
+		computed: {
+			durationText() {
 				return util.formatDuration(this.duration);
-			}
+			},
+			...mapState(['userInfo'])
 		},
 		components: {
-			nxButton
+			nxButton,
+			wmPoster
 		}
 	}
 </script>
@@ -88,8 +196,8 @@
 		width: 300upx;
 		height: 300upx;
 	}
-	
-	.group{
+
+	.group {
 		padding: 0 90upx;
 	}
 
